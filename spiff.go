c6568ca1f265e4a1da7a0a8b44d77b7e71ba0832
Alex Suraci <suraci.alex@gmail.com>
2014-03-01 18:02:26 -0800

    bump to 1.0.0
    major version bump as the semantics of merging lists has changed in
    a backwards-incompatible way. see 83c0aa.

--- spiff.go
+++ spiff.go
@@ -20,7 +20,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "spiff"
 	app.Usage = "BOSH deployment manifest toolkit"
-	app.Version = "0.3.0"
+	app.Version = "1.0.0"
 
 	app.Commands = []cli.Command{
 		{
