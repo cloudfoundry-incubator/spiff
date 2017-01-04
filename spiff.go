015451e8631b3b880c409fd0c4f829b3b8ebdd5c
Joshua Aresty <joshua.aresty@emc.com>
2017-01-04 09:01:34 -0800

    [#133145805] Move Godeps into vendor and increment spiff version to enable compilation with go 1.7.4

--- spiff.go
+++ spiff.go
@@ -20,7 +20,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "spiff"
 	app.Usage = "BOSH deployment manifest toolkit"
-	app.Version = "1.0.7"
+	app.Version = "1.0.8"
 
 	app.Commands = []cli.Command{
 		{
